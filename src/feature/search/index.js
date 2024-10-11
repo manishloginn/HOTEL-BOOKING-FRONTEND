e88fcfe22d263f3d9bc7651983dbe00ef113aa04
manishloginn <manishv7714@gmail.com>
2024-10-11 17:13:08 +0530

    more detail fill

--- src/feature/search/index.js
+++ src/feature/search/index.js
@@ -1,40 +1,97 @@
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import { useState } from "react";
-import "./style/page.scss"
+import "./style/page.scss";
 
 
-import { UsergroupDeleteOutlined } from '@ant-design/icons';
+import { Calendar, theme } from 'antd';
+
+
+
+import { LineOutlined, UsergroupDeleteOutlined } from '@ant-design/icons';
+import dayjs from "dayjs";
+
 
 
 const CitySearch = () => {
 
+  const { token } = theme.useToken();
+
   const [formData, setformData] = useState({
     location: '',
-    checkindate: "",
-    checkoutdate: "",
+    checkindate: dayjs().format('YYYY-MM-DD'),
+    checkoutdate: dayjs().add(1, 'day').format('YYYY-MM-DD'),
     guest: 1
   })
+  const [checkinTrue, setcheckinTrue] = useState(false)
+  const [checkOutTrue, setcheckOutTrue] = useState(false)
 
   const navigate = useNavigate()
 
 
+
+
   const handelclick = () => {
-    navigate(`/search/${formData.location}/${formData.checkindate}/${formData.checkoutdate}/${formData.guest}`)
-    // navigate('/search/:location/:checkindate/:checkoutdate/:guest')
+    if (!formData.location || !formData.checkindate || !formData.checkoutdate || !formData.guest) {
+      alert('please fill all detail')
+    } else {
+      navigate(`search/${formData.location}/${formData.checkindate}/${formData.checkoutdate}/${formData.guest}`)
+    }
   }
 
 
 
   const formchangeHandel = (e) => {
     const { name, value } = e.target;
-    setformData({
-      ...formData,
+    setformData((prev) => ({
+      ...prev,
       [name]: value
-    })
+    }));
+  }
+
+  const onCheckinChange = (value) => {
+    const date = value.format('YYYY-MM-DD');
+    console.log('Selected Check-out Date:', date);
+    setformData((prev) => ({ ...prev, checkindate: date }));
+  };
+
+  const onCheckOutChange = (value) => {
+    const date = value.format('YYYY-MM-DD');
+    setformData((prev) => ({ ...prev, checkoutdate: date }));
+  };
+
+  const haldelcheckinCalender = () => {
+    if (checkOutTrue) {
+      setcheckOutTrue(false)
+    }
+    setTimeout(() => {
+      setcheckinTrue((prev) => !prev)
+    }, 100);
+
+  }
+
+  const haldelCheckoutCalender = () => {
+    if (checkinTrue) {
+      setcheckinTrue(false)
+    }
+    setTimeout(() => {
+      setcheckOutTrue((prev) => !prev)
+    }, 100);
+
   }
 
   console.log(formData)
+
+
+
+
+  const wrapperStyle = {
+    width: 300,
+    border: `1px solid ${token.colorBorderSecondary}`,
+    borderRadius: token.borderRadiusLG,
+    position: "absolute",
+    top: "370px",   // Adjust as needed
+  };
 
   return (
     <div>
@@ -44,26 +101,37 @@ const CitySearch = () => {
 
       <div className="inputcontainer">
         <div className="inputfields">
-          <input
-            style={{ width: "200px" }}
+          <input className="cityname"
             type="text"
             name="location"
             placeholder="Search City"
-            required
             onChange={formchangeHandel}
           />
-          <input
-            type="date"
-            name="checkindate"
-            required
-            onChange={formchangeHandel}
-          />
-          <input
-            type="date"
-            name="checkoutdate"
-            required
-            onChange={formchangeHandel}
-          />
+          <div className="calender">
+            <p onClick={haldelcheckinCalender}>{formData.checkindate} </p>
+            {
+              checkinTrue && <div style={wrapperStyle}>
+                <Calendar
+                  fullscreen={false}
+                  onChange={onCheckinChange}
+                  disabledDate={(current) => current && current < dayjs().startOf('day')}
+                />
+              </div>
+            }
+            <LineOutlined />
+            <p onClick={haldelCheckoutCalender}>{formData.checkoutdate}</p>
+            {
+              checkOutTrue && <div style={wrapperStyle}>
+                <Calendar
+                  fullscreen={false}
+                  onChange={onCheckOutChange}
+                  disabledDate={(current) => current && current < dayjs().startOf('day')}
+                />
+              </div>
+            }
+          </div>
+
+
           <label className="inputlabel">
             <input
               style={{ width: "30px" }}
@@ -76,7 +144,7 @@ const CitySearch = () => {
             <UsergroupDeleteOutlined />
           </label>
 
-          <button  onClick={handelclick}>Search</button>
+          <button onClick={handelclick}>Search</button>
         </div>
 
 
